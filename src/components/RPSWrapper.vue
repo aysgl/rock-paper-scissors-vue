e872a142c82e057f9213e49df4a052717b74135c
Ayşegül Avcu <aysegul.avcu@hotmail.com>
2025-10-17 17:31:11 +0300

    rps wrapper step 1

--- src/components/RPSWrapper.vue
+++ src/components/RPSWrapper.vue
@@ -2,18 +2,23 @@
 import PaperSvg from './svg/PaperSvg.vue'
 import RockSvg from './svg/RockSvg.vue'
 import ScissorsSvg from './svg/ScissorsSvg.vue'
+import TriangleSvg from './svg/TriangleSvg.vue'
 </script>
 
 <template>
   <div class="rps-wrapper">
-    <div class="rps-wrapper-item">
-      <RockSvg />
+    <div class="rps-wrapper-triangle">
+      <TriangleSvg />
     </div>
-    <div class="rps-wrapper-item">
+
+    <div class="rps-wrapper-item paper">
       <PaperSvg />
     </div>
-    <div class="rps-wrapper-item">
+    <div class="rps-wrapper-item scissors">
       <ScissorsSvg />
+    </div>
+    <div class="rps-wrapper-item rock">
+      <RockSvg />
     </div>
   </div>
 </template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import LeadCard from './LeadCard.vue'
+import RPSWrapper from './RPSWrapper.vue'
 </script>
 
 <template>
   <LeadCard />
+  <RPSWrapper />
 </template>
