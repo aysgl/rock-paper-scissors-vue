5fd9718ea33e8c2643210ec13a1d7b75fb3953f6
Ayşegül Avcu <aysegul.avcu@hotmail.com>
2025-10-17 17:00:54 +0300

    Stil dosyasının scss e çevrilmesi ve LeadCard componentin tasarımı

---
+++ src/components/LeadCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TextSvg from './svg/TextSvg.vue'
+</script>
+
+<template>
+  <div class="lead">
+    <TextSvg />
+
+    <div class="lead-score">
+      <span>Score</span>
+      <h1>12</h1>
+    </div>
+  </div>
+</template>
---
+++ src/components/RPSWrapper.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import PaperSvg from './svg/PaperSvg.vue'
+import RockSvg from './svg/RockSvg.vue'
+import ScissorsSvg from './svg/ScissorsSvg.vue'
+</script>
+
+<template>
+  <div class="rps-wrapper">
+    <div class="rps-wrapper-item">
+      <RockSvg />
+    </div>
+    <div class="rps-wrapper-item">
+      <PaperSvg />
+    </div>
+    <div class="rps-wrapper-item">
+      <ScissorsSvg />
+    </div>
+  </div>
+</template>
